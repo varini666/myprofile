61a6ab5ff011499cfee99ff2a5adf471d1de62ee
varini666 <varinivarini925@gmail.com>
2026-05-04 09:07:08 +0530

    skills data

--- js/data/skills.js
+++ js/data/skills.js
@@ -1,27 +1,37 @@
-
-const skillsData = [
-  {
-    id: 1,
-    name: "ReactJS",
-    shortLabel: "R",
-    description: "Advanced Hooks, component-based UI, and state management."
-  },
-  {
-    id: 2,
-    name: "Tailwind CSS",
-    shortLabel: "T",
-    description: "Responsive layouts, utility-first styling, and clean UI building."
-  },
-  {
-    id: 3,
-    name: "JavaScript",
-    shortLabel: "J",
-    description: "DOM manipulation, events, validation, Fetch API, and storage."
-  },
-  {
-    id: 4,
-    name: "MongoDB",
-    shortLabel: "M",
-    description: "Document-based database design and backend integration."
-  }
+export const skillsData = [
+    {
+        id: 1,
+        name: "ReactJS",
+        description: "Advanced Hooks, component-based UI, and state management."
+    },
+    {
+        id: 2,
+        name: "Tailwind CSS",
+        description: "Responsive layouts, utility-first styling, and clean UI building."
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        description: "DOM manipulation, events, validation, Fetch API, and storage."
+    },
+    {
+        id: 4,
+        name: "MongoDB",
+        description: "Document-based database design and backend integration."
+    },
+    {
+        id: 5,
+        name: "C",
+        description: "C is a general-purpose programming language used to build system software and applications."
+    },
+    {
+        id: 6,
+        name: "C++",
+        description: "C++ is an extension of C that supports both procedural and object-oriented programming."
+    },
+    {
+        id: 7,
+        name: "Python",
+        description: "Python is a high-level, easy-to-learn language used for building a wide range of applications."
+    }
 ];
