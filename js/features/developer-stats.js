c9e62413785243a40e2156a0022f9bef616c70c8
varini666 <varinivarini925@gmail.com>
2026-05-04 09:07:54 +0530

    developer-states

---
+++ js/features/developer-stats.js
@@ -0,0 +1,8 @@
+export function renderDeveloperStats() {
+    const el = document.getElementById("developerStats");
+    el.innerHTML = `
+    <div class="stat-box"><h3>4</h3><p>Projects</p></div>
+    <div class="stat-box"><h3>7</h3><p>Core Skills</p></div>
+    <div class="stat-box"><h3>100%</h3><p>Passion</p></div>
+    `;
+}
