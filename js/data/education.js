b07a06ca883b97b9193bdb73104773bc1f498a5b
varini666 <varinivarini925@gmail.com>
2026-05-04 09:06:11 +0530

    education

---
+++ js/data/education.js
@@ -0,0 +1,29 @@
+export const educationData = [
+    {
+        id: 1,
+        title: "B.E in Computer Science",
+        institution: "CIT Mandya",
+        board: "VTU",
+        year: "2026",
+        scoreLabel: "CGPA",
+        score: "8.53"
+    },
+    {
+        id: 2,
+        title: "Pre-University",
+        institution: "NPA",
+        board: "PCMB",
+        year: "2022",
+        scoreLabel: "Percentage",
+        score: "74.33%"
+    },
+    {
+        id: 3,
+        title: "SSLC",
+        institution: "L.E.S",
+        board: "Karnataka Secondary Board",
+        year: "2020",
+        scoreLabel: "Percentage",
+        score: "79.55%"
+    }
+];
